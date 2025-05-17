0ae49506dccc4f409bb4d7bedd203d8ab57ad0e5
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-17 15:12:28 +0700

    Cetak data semua objek dan akses static function getNim

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -39,3 +39,12 @@ int main() {
     mahasiswa mhs3("Andi Janu");
     mahasiswa mhs4("Joko Wahono");
 
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "Akses dari luar object = " << mahasiswa::getNim() << endl; // mengakses nim melalui static member function "getNim"
+
+    return 0;
+}
