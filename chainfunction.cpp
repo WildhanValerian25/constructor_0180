8d93db3cf9c863ef17dc69639d23f43a2148660c
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-17 14:44:19 +0700

    Gunakan method chaining untuk mengatur dan menampilkan judul

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -21,3 +21,7 @@ public:
 int main()
 {
     // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
